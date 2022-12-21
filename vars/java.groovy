341d88883e94406c093b08d7eb225d679bc91b9e
Divya <divya5guntaka@gmail.com>
2022-12-21 11:16:51 +0530

    Update jenkins

--- vars/java.groovy
+++ vars/java.groovy
@@ -1,3 +1,34 @@
-@Library('roboshop') _
+def call() {
+    pipeline {
+        agent any
 
-java()
+        stages {
+
+            stage('Compile') {
+                steps {
+                    echo 'Compile'
+                }
+            }
+
+            stage('Code Quality') {
+                steps {
+                    echo 'Code Quality'
+                }
+            }
+
+            stage('Test Cases') {
+                steps {
+                    echo 'Test Cases'
+                }
+            }
+
+            stage('Publish A Release') {
+                steps {
+                    echo 'Publish A Release'
+                }
+            }
+
+        }
+
+    }
+}
