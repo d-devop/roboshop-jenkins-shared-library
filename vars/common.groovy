b26ada7ed29a92e0106f15fa085e435e1c4b68f2
Divya <divya5guntaka@gmail.com>
2022-12-23 11:00:39 +0530

    Update jenkins

--- vars/common.groovy
+++ vars/common.groovy
@@ -33,4 +33,5 @@ def release() {
 
 def mail() {
     mail bcc: '', body: "<h1>Pipeline Failure</h1><br>Project Name: ${COMPONENT}\nURL = ${BUILD_URL}", cc: '', charset: 'UTF-8', from: 'divya5guntaka@gmail.com', mimeType: 'text/html', replyTo: 'divya5guntaka@gmail.com', subject: "ERROR CI: Component Name - ${COMPONENT}", to: "divya5guntaka@gmail.com"
+    sh 'exit 1'
 }
