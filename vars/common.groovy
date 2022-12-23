a31a7766a4f2a11248c07ec77cf64face5496c59
Divya <divya5guntaka@gmail.com>
2022-12-23 15:04:04 +0530

    Update jenkins

--- vars/common.groovy
+++ vars/common.groovy
@@ -46,10 +46,14 @@ def codeQuality() {
     }
 }
 
-def release() {
+def release(appType) {
     stage('Publish A Release') {
-        echo 'Publish A Release'
-        sh 'env'
+        if (appType == "nodejs") {
+            sh '''
+              npm install 
+              zip -r ${COMPONENT}-${TAG_NAME}.zip node_modules server.js schema
+            '''
+        }
     }
 }
 
